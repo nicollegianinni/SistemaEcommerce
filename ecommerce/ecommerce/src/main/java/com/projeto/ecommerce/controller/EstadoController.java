0247e1f09fee67f01477f0926afccb3062c2a138
NICHOLLE GIANINNI <gianinni.nicolle@gmail.com>
2025-06-19 14:29:44 -0300

    metodo de cadastrar e salvar

--- ecommerce/ecommerce/src/main/java/com/projeto/ecommerce/controller/EstadoController.java
+++ ecommerce/ecommerce/src/main/java/com/projeto/ecommerce/controller/EstadoController.java
@@ -3,14 +3,14 @@ package com.projeto.ecommerce.controller;
 import com.projeto.ecommerce.domain.Estado;
 import com.projeto.ecommerce.repository.EstadoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@RestController
-@RequestMapping("EstadoController")
+@Controller
 public class EstadoController {
 
     @Autowired
@@ -23,8 +23,9 @@ public class EstadoController {
         return mv;
     }
     @PostMapping("/salvarEstado")
-    public void salvarEstado (Estado estado){
-
+    public ModelAndView salvar (Estado estado){
+        ModelAndView mv = new ModelAndView("administrativo/estados/cadastro/salvar");
+        return mv;
     }
 }
 
