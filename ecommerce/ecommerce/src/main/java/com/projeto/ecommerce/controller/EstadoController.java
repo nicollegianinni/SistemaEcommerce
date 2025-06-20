e5ddc1a1b93c959ea515681b3cf3d7dfcf2778e6
NICHOLLE GIANINNI <gianinni.nicolle@gmail.com>
2025-06-20 01:39:27 -0300

    ajustando html

--- ecommerce/ecommerce/src/main/java/com/projeto/ecommerce/controller/EstadoController.java
+++ ecommerce/ecommerce/src/main/java/com/projeto/ecommerce/controller/EstadoController.java
@@ -4,29 +4,42 @@ import com.projeto.ecommerce.domain.Estado;
 import com.projeto.ecommerce.repository.EstadoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+
 @Controller
+
 public class EstadoController {
 
     @Autowired
     private EstadoRepository estadoRepository;
 
     @GetMapping("/cadastroEstado")
-    public ModelAndView Cadastrar(Estado estado) {
+    public ModelAndView cadastrar(Estado estado) {
         ModelAndView mv = new ModelAndView("administrativo/estados/cadastro");
-        mv.addObject("estado", estado);
+        mv.addObject ("estado", estado);
         return mv;
     }
+
     @PostMapping("/salvarEstado")
-    public ModelAndView salvar (Estado estado){
-        ModelAndView mv = new ModelAndView("administrativo/estados/cadastro/salvar");
-        return mv;
+    public ModelAndView salvar(Estado estado , BindingResult result) {
+        if (result.hasErrors()){
+            return cadastrar(estado);
+        }
+        estadoRepository.saveAndFlush(estado);
+        return cadastrar(new Estado());
     }
+
+//    @GetMapping ("/Listar")
+//    public ModelAndView Listar(Estado estado1){
+//        ModelAndView list = new ModelAndView("administrativo/lista");
+//        list.addObject("estado" , estado1);
+//        return list;
+//    }
+
 }
 
 
